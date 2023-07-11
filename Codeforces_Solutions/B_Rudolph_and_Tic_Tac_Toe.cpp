0f161ae01cf466ac83b2c5b2e6a3f248d4d700c0
Tashin Parvez <tashinparvez2002@gmail.com>
2023-07-11 23:13:06 +0600

    Subtraction Game CPP Codeforces Codeforces Round 884 (Div. 1 + Div. 2)

--- Codeforces_Solutions/B_Rudolph_and_Tic_Tac_Toe.cpp
+++ Codeforces_Solutions/B_Rudolph_and_Tic_Tac_Toe.cpp
@@ -3,10 +3,10 @@
 //        ****************  Updated:    21-06-23     *************************\
 
 #include <bits/stdc++.h>
-#define faster                        \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(0);                       \
-    cout.tie(0);
+#define faster                          \
+      ios_base::sync_with_stdio(false); \
+      cin.tie(0);                       \
+      cout.tie(0);
 #define Read(x) freopen(x, "r", stdin)
 #define Write(x) freopen(x, "w", stdout)
 #define CRACKED return 0;
@@ -22,13 +22,13 @@
 // I/O :
 #define loop(n) for (int i = 0; i < n; i++) // 0 to n Input or Output
 #define output(x) cout << x << nl           // out
-#define printarray(arr, len)      \
-    for (int i = 0; i < len; i++) \
-    {                             \
-        cout << arr[i] << " ";    \
-        if (i + 1 == len)         \
-            cout << endl;         \
-    } // array print
+#define printarray(arr, len)        \
+      for (int i = 0; i < len; i++) \
+      {                             \
+            cout << arr[i] << " ";  \
+            if (i + 1 == len)       \
+                  cout << endl;     \
+      } // array print
 
 #define sq(x) ((x) * (x)) // x^2  square
 #define len(s) s.length()
@@ -54,82 +54,82 @@ using namespace std;
 
 void solution()
 {
-    int i, j, k, l, m, n, a, b, c, d, w, x, y, z, t, count = 0, index;
-    string s;
-    bool flag = false;
-    char arr[4][4];
-    for (int i = 1; i < 4; i++)
-    {
-        for (int j = 1; j < 4; j++)
-        {
-            cin >> arr[i][j];
-        }
-    }
-    string ans = "DRAW";
-    if (arr[1][1] != '.')
-    {
+      int i, j, k, l, m, n, a, b, c, d, w, x, y, z, t, count = 0, index;
+      string s;
+      bool flag = false;
+      char arr[4][4];
+      for (int i = 1; i < 4; i++)
+      {
+            for (int j = 1; j < 4; j++)
+            {
+                  cin >> arr[i][j];
+            }
+      }
+      string ans = "DRAW";
+      if (arr[1][1] != '.')
+      {
 
-        if (arr[1][1] == arr[1][2] && arr[1][1] == arr[1][3])
-        {
-            ans = arr[1][1];
-        }
+            if (arr[1][1] == arr[1][2] && arr[1][1] == arr[1][3])
+            {
+                  ans = arr[1][1];
+            }
 
-        if (arr[2][1] == arr[1][1] && arr[1][1] == arr[3][1])
-        {
-            ans = arr[1][1];
-        }
+            if (arr[2][1] == arr[1][1] && arr[1][1] == arr[3][1])
+            {
+                  ans = arr[1][1];
+            }
 
-        if (arr[1][1] == arr[2][2] && arr[1][1] == arr[3][3])
-        {
-            ans = arr[1][1];
-        }
-    }
+            if (arr[1][1] == arr[2][2] && arr[1][1] == arr[3][3])
+            {
+                  ans = arr[1][1];
+            }
+      }
 
-    if (arr[1][2] != '.' && arr[1][2] == arr[2][2] && arr[1][2] == arr[3][2])
-    {
-        ans = arr[1][2];
-    }
+      if (arr[1][2] != '.' && arr[1][2] == arr[2][2] && arr[1][2] == arr[3][2])
+      {
+            ans = arr[1][2];
+      }
 
-    if (arr[1][3] != '.' && arr[1][3] == arr[2][3] && arr[1][3] == arr[3][3])
-    {
-        ans = arr[1][3];
-    }
+      if (arr[1][3] != '.' && arr[1][3] == arr[2][3] && arr[1][3] == arr[3][3])
+      {
+            ans = arr[1][3];
+      }
 
-    if (arr[1][3] != '.' && arr[1][3] == arr[2][2] && arr[1][3] == arr[3][1])
-    {
-        ans = arr[1][3];
-    }
+      if (arr[1][3] != '.' && arr[1][3] == arr[2][2] && arr[1][3] == arr[3][1])
+      {
+            ans = arr[1][3];
+      }
 
-    if (arr[2][1] != '.' && arr[2][1] == arr[2][2] && arr[2][1] == arr[2][3])
-    {
-        ans = arr[2][1];
-    }
+      if (arr[2][1] != '.' && arr[2][1] == arr[2][2] && arr[2][1] == arr[2][3])
+      {
+            ans = arr[2][1];
+      }
 
-    if (arr[3][1] != '.' && arr[3][1] == arr[3][2] && arr[3][1] == arr[3][3])
-    {
-        ans = arr[3][1];
-    }
-    cout << ans << nl;
+      if (arr[3][1] != '.' && arr[3][1] == arr[3][2] && arr[3][1] == arr[3][3])
+      {
+            ans = arr[3][1];
+      }
+      cout << ans << nl;
 }
 
 int32_t main()
 {
-    faster;
+      faster;
 
-    // #ifdef TashinParvez
-    //     Read("input.txt");
-    //   //  Write("output.txt");
-    // #endif // TashinParvez
+      // #ifdef TashinParvez
+      //     Read("input.txt");
+      //   //  Write("output.txt");
+      // #endif // TashinParvez
 
-    int t = 1;
-    cin >> t;
-    int c = 1;
+      int t = 1;
+      cin >> t;
+      int c = 1;
 
-    while (t--)
-    {
-        // cout << "Case " << c++ << ": ";
-        solution();
-    }
+      while (t--)
+      {
+            // cout << "Case " << c++ << ": ";
+            solution();
+      }
 
-    CRACKED;
+      CRACKED;
 }
