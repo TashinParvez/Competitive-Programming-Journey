a52a9b7df6c3f4f6bc32e7e2da0d1e1d44257b39
Tashin Parvez <tashinparvez2002@gmail.com>
2023-07-20 20:53:10 +0600

    Solved Round down price

---
+++ Codeforces_Solutions/A_Round_Down_the_Price.cpp
@@ -0,0 +1,108 @@
+
+//        ****************  Author :  Tashin.Parvez  *************************\
+//        ****************  Updated:    21-06-23     *************************\
+
+#include <bits/stdc++.h>
+#define faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define Read(x) freopen(x, "r", stdin)
+#define Write(x) freopen(x, "w", stdout)
+#define CRACKED return 0;
+#define nl "\n"
+
+// data_type_compressions :
+#define int long long
+#define ull unsigned long long
+#define ld long double
+
+#define PI 3.14159265358979323846
+
+// I/O :
+#define loop(n) for (int i = 0; i < n; i++) // 0 to n Input or Output
+#define output(x) cout << x << nl           // out
+#define printarray(arr, len)      \
+    for (int i = 0; i < len; i++) \
+    {                             \
+        cout << arr[i] << " ";    \
+        if (i + 1 == len)         \
+            cout << endl;         \
+    } // array print
+
+#define sq(x) ((x) * (x)) // x^2  square
+#define len(s) s.length()
+#define setDec(x) fixed << setprecision(x)
+
+#define YES printf("YES\n")
+#define Yes printf("Yes\n")
+#define NO printf("NO\n")
+#define No printf("No\n")
+
+using namespace std;
+
+/*
+    int n; cin>>n;
+    int arr[n];
+    for(auto &v : arr) cin>>v;
+
+
+    if(!(i&1))   //-------------- EVEN  (for even --->> i&1 == 0)
+        cout<<i<<nl;
+
+*/
+
+void solution()
+{
+    int i, j, k, l, m, n, a, b, c, d, w, x, y, z, t, count = 0, index;
+    string s;
+    bool flag = false;
+
+    cin >> n;
+    s = to_string(n);
+    char chr = s[0];
+
+    if (chr >= '2' && chr <= '9')
+    {
+        chr--;
+        cout << chr;
+        flag = true;
+    }
+
+    for (int i = 1; i < s.length(); i++)
+    {
+        if (s[i] == '0' && flag == false)
+        {
+            continue;
+        }
+        cout << s[i];
+        flag = true;
+    }
+    // Round down price
+    //  cout << "            " << chr << nl;
+    //  cout <<"            "<<n<< nl;
+    //  cout <<"            "<<s<< nl;
+    if (flag == false)
+    {
+        cout << 0;
+    }
+
+    cout << nl;
+}
+
+int32_t main()
+{
+    faster;
+
+    int t = 1;
+    cin >> t;
+    int c = 1;
+
+    while (t--)
+    {
+        // cout << "Case " << c++ << ": ";
+        solution();
+    }
+
+    CRACKED;
+}
