b6398c7a662fb4f937338da963574d8dd9d962c9
Tashin Parvez <tashinparvez2002@gmail.com>
2023-07-29 16:19:11 +0600

    solved some Assiut loop problems {basic}

---
+++ Codeforces_Solutions/G_Katryoshka.cpp
@@ -0,0 +1,103 @@
+
+//        ****************  Author :  Tashin.Parvez  *************************\
+//        ****************  Updated:    21-06-23     *************************\
+
+#include <bits/stdc++.h>
+#define faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define Read(x) freopen(x, "r", stdin)
+#define Write(x) freopen(x, "w", stdout)
+#define CRACKED return 0;
+#define nl "\n"
+
+// data_type_compressions :
+#define int long long
+#define ull unsigned long long
+#define ld long double
+
+#define PI 3.14159265358979323846
+
+// I/O :
+#define loop(n) for (int i = 0; i < n; i++) // 0 to n Input or Output
+#define output(x) cout << x << nl           // out
+#define printarray(arr, len)      \
+    for (int i = 0; i < len; i++) \
+    {                             \
+        cout << arr[i] << " ";    \
+        if (i + 1 == len)         \
+            cout << endl;         \
+    } // array print
+
+#define sq(x) ((x) * (x)) // x^2  square
+#define len(s) s.length()
+#define setDec(x) fixed << setprecision(x)
+
+#define YES printf("YES\n")
+#define Yes printf("Yes\n")
+#define NO printf("NO\n")
+#define No printf("No\n")
+
+using namespace std;
+
+/*
+    int n; cin>>n;
+    int arr[n];
+    for(auto &v : arr) cin>>v;
+
+
+    if(!(i&1))   //-------------- EVEN  (for even --->> i&1 == 0)
+        cout<<i<<nl;
+
+*/
+
+void solution()
+{
+    int i, j, k, l, m, n, a, b, e, c, d, w, x, y, z, t, count = 0, index;
+    string s;
+    bool flag = false;
+
+    cin >> e >> m >> b;
+    int cnt = 0;
+
+    int mi = min(e, min(m, b));
+
+    cnt += mi;
+    m -= mi;
+    b -= mi;
+    e -= mi;
+    if (b == 0 || e == 0)
+    {
+        cout << cnt << nl;
+        return;
+    }
+    k = e / 2;
+    if (k <= b)
+        cnt += k;
+    else
+        cnt += b;
+
+    cout << cnt << endl;
+}
+
+int32_t main()
+{
+    faster;
+
+    // #ifdef TashinParvez
+    //     Read("input.txt");
+    //   //  Write("output.txt");
+    // #endif // TashinParvez
+
+    // int t = 1; cin >> t;
+    // int c = 1;
+
+    // while (t--)
+    {
+        // cout << "Case " << c++ << ": ";
+        solution();
+    }
+
+    CRACKED;
+}
