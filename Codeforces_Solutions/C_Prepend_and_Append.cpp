ade0d02ca10d14e0affa1f426c4c2e363d6bde71
Tashin Parvez <tashinparvez2002@gmail.com>
2023-10-24 10:51:23 +0600

    Day-2 end Finished Goal of Div 4 Solve 4

---
+++ Codeforces_Solutions/C_Prepend_and_Append.cpp
@@ -0,0 +1,106 @@
+
+//        ****************  Author :  Tashin.Parvez  *************************\
+//        ****************  Updated:    21-06-23     *************************\
+
+#include <bits/stdc++.h>
+#define faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define Read(x) freopen(x, "r", stdin)
+#define Write(x) freopen(x, "w", stdout)
+#define CRACKED return 0;
+#define nl "\n"
+
+// data_type_compressions :
+#define int long long
+#define ull unsigned long long
+#define ld long double
+
+#define PI 3.14159265358979323846
+
+// I/O :
+#define loop(n) for (int i = 0; i < n; i++) // 0 to n Input or Output
+#define output(x) cout << x << nl           // out
+#define printarray(arr, len)      \
+    for (int i = 0; i < len; i++) \
+    {                             \
+        cout << arr[i] << " ";    \
+        if (i + 1 == len)         \
+            cout << endl;         \
+    } // array print
+
+#define sq(x) ((x) * (x)) // x^2  square
+#define len(s) s.length()
+#define setDec(x) fixed << setprecision(x)
+
+#define YES printf("YES\n")
+#define Yes printf("Yes\n")
+#define NO printf("NO\n")
+#define No printf("No\n")
+
+using namespace std;
+
+/*
+    int n; cin>>n;
+    int arr[n];
+    for(auto &v : arr) cin>>v;
+
+
+    if(!(i&1))   //-------------- EVEN  (for even --->> i&1 == 0)
+        cout<<i<<nl;
+
+*/
+
+void solution()
+{
+    int i, j, k, l, m, n, a, b, c, d, w, x, y, z, t, count = 0, index;
+    string str;
+    bool flag = false;
+
+    cin >> n >> str;
+    int s = 0, e = n - 1;
+    count = 0;
+    while (1)
+    {
+        if (s > e)
+            break;
+        if (str[s] == '0' && str[e] == '1')
+        {
+            count += 2;
+            s++;
+            e--;
+        }
+        else if (str[s] == '1' && str[e] == '0')
+        {
+            count += 2;
+            s++;
+            e--;
+        }
+        else
+            break;
+    }
+    cout << n - count << nl;
+}
+
+int32_t main()
+{
+    faster;
+
+    // #ifdef TashinParvez
+    //     Read("input.txt");
+    //   //  Write("output.txt");
+    // #endif // TashinParvez
+
+    int t = 1;
+    cin >> t;
+    int c = 1;
+
+    while (t--)
+    {
+        // cout << "Case " << c++ << ": ";
+        solution();
+    }
+
+    CRACKED;
+}
---
+++ Codeforces_Solutions/D_Distinct_Split.cpp
@@ -0,0 +1,107 @@
+
+//        ****************  Author :  Tashin.Parvez  *************************\
+//        ****************  Updated:    21-06-23     *************************\
+
+#include <bits/stdc++.h>
+#define faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define Read(x) freopen(x, "r", stdin)
+#define Write(x) freopen(x, "w", stdout)
+#define CRACKED return 0;
+#define nl "\n"
+
+// data_type_compressions :
+#define int long long
+#define ull unsigned long long
+#define ld long double
+
+#define PI 3.14159265358979323846
+
+// I/O :
+#define loop(n) for (int i = 0; i < n; i++) // 0 to n Input or Output
+#define output(x) cout << x << nl           // out
+#define printarray(arr, len)      \
+    for (int i = 0; i < len; i++) \
+    {                             \
+        cout << arr[i] << " ";    \
+        if (i + 1 == len)         \
+            cout << endl;         \
+    } // array print
+
+#define sq(x) ((x) * (x)) // x^2  square
+#define len(s) s.length()
+#define setDec(x) fixed << setprecision(x)
+
+#define YES printf("YES\n")
+#define Yes printf("Yes\n")
+#define NO printf("NO\n")
+#define No printf("No\n")
+
+using namespace std;
+
+/*
+    int n; cin>>n;
+    int arr[n];
+    for(auto &v : arr) cin>>v;
+
+
+    if(!(i&1))   //-------------- EVEN  (for even --->> i&1 == 0)
+        cout<<i<<nl;
+
+*/
+
+void solution3()
+{
+    int i, j, k, l, m, n, a, b, c, d, w, x, y, z, t, count = 0, index;
+    string s, str1 = "", str2 = "";
+    bool flag = false;
+    cin >> n >> s;
+
+    int p = 0;
+    int arr[26] = {0};
+    int pre[n] = {0};
+
+    int su = 0;
+    int arr2[26] = {0};
+    int suf[n] = {0};
+
+    for (int i = 0, j = n - 1; i < n; j--, i++)
+    {
+        // pre
+        arr[s[i] - 'a']++;
+        if (arr[s[i] - 'a'] == 1)
+            p++;
+        pre[i] = p;
+
+        // suff
+        arr2[s[j] - 'a']++;
+        if (arr2[s[j] - 'a'] == 1)
+            su++;
+        suf[j] = su;
+    }
+
+    int mx = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        mx = max(mx, pre[i] + suf[i + 1]);
+    }
+    cout << mx << nl;
+}
+
+int32_t main()
+{
+    faster;
+
+    int t = 1;
+    cin >> t;
+    int c = 1;
+
+    while (t--)
+    {
+        solution3();
+    }
+
+    CRACKED;
+}
