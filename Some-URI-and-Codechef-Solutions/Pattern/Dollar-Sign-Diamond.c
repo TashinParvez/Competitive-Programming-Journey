376aafc8f948c8fcf1ff8bce845536fd53c8da7b
Tashin Parvez <tashinparvez2002@gmail.com>
2024-02-22 11:08:13 +0600

    Updates

---
+++ Some-URI-and-Codechef-Solutions/Pattern/Dollar-Sign-Diamond.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+
+    for(int i= 1;i<n/2;i++)
+    {
+
+    }
+
+    for(int i= 1;i<n;i++)
+    {
+
+    }
+
+    for(int i= 1;i<n/2;i++)
+    {
+
+    }
+    
+    return 0;
+}
